5f8d45e50233eedd99eea557ac49ca40d624e8a9
Alexander Sosov <nookat.develop@gmail.com>
2023-04-30 18:41:56 +0300

    learn: дженерик-и-функции

---
+++ 07-generics/02-дженерик-и-функция.ts
@@ -0,0 +1,13 @@
+function logMiddleware<T>(data: T): T {
+  console.log(data);
+  return data;
+}
+
+const res1 = logMiddleware<number>(10);
+
+function getSplitedHalf<T>(data: Array<T>): Array<T> {
+  const l = data.length / 2;
+  return data.splice(0, l);
+}
+
+console.log(getSplitedHalf<number>([1, 3, 4, 6]))
